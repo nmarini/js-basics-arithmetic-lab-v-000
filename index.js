f6416fc1b712fbae66115b1620a86b845478f4b3
Nicolas Marini <nmarini44@gmail.com>
2019-04-09 23:10:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,4 +13,4 @@ const ageIsValid = Number.isInteger(currentAge);
 
 const randomNumber = (Math.random() * 20);
 
-const randomInteger = Math.floo(randomNumber)
+const randomInteger = Math.floor(randomNumber)
